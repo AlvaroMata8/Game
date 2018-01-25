b272f0e3a76c8f3d7a710c447b3950b97cd8f14f
AlvaroMata8 <alvaromataaracil@hotmail.com>
2018-01-25 18:58:50 +0100

    intentando colisionar con vectores

--- js/main.js
+++ js/main.js
@@ -11,11 +11,10 @@ var ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     player.render();
     player.updateChar();
+    player.collision();
     player.moveOrder();
     drawObstacles()
     requestAnimationFrame(update);
-
-    
   };
   
   requestAnimationFrame(update);
--- js/player.js
+++ js/player.js
@@ -68,6 +68,43 @@ Character.prototype.moveOrder = function () {
   }
 }
 
+Character.prototype.collision = function () {
+  
+  var that = this;
+    object.forEach(function(){
+
+  var vectorX = (that.x + (that.width / 2)) -  (object.x +  (object.width / 2));
+  var vectorY = (that.y + (that.height / 2)) -  (object.y +  (object.height / 2));
+
+  var halfWidths = (that.width / 2) +  (object.width / 2);
+  var halfHeights = (that.height / 2) +  (object.height / 2);
+
+  var collisionDirection = null;
+
+  if (Math.abs(vectorX) < halfWidths && Math.abs(vectorY) < halfHeights) {
+    var offsetX = halfWidths - Math.abs(vectorX);
+    var offsetY = halfHeights - Math.abs(vectorY);
+    if (offsetX < offsetY) {
+      if (vectorX > 0) {
+        collisionDirection = "left";
+        that.x += offsetX;
+      } else {
+        ollisionDirection = "right";
+        that.x -= offsetX;
+      }
+    } else {
+      if (vectorY > 0) {
+        collisionDirection = "top";
+        that.y += offsetY;
+      } else {
+        collisionDirection = "bottom";
+        that.y -= offsetY;
+      }
+    }
+  };
+  return collisionDirection;
+})
+};
 
 // si la posicion player X e Y es igual a la posicion
 // de algun objeto del Array obstaculos
